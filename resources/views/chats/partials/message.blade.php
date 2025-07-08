392fe0295ac17fdf158de0c5af6285bf16b9ff7d
Alex <alex@nebulasys.info>
2025-07-08 22:13:10 +0200

    Make message box boundaries fit content

--- resources/views/chats/partials/message.blade.php
+++ resources/views/chats/partials/message.blade.php
@@ -3,13 +3,13 @@
     @if($messageOwned)
         data-owned="true"
     @endif
-    class="relative p-2 mb-10 rounded-md bg-neutral-200 data-[owned]:bg-blue-200 data-[owned]:left-full data-[owned]:-translate-x-full max-w-2/3"
+    class="relative p-2 mb-10 rounded-md bg-neutral-200 data-[owned]:bg-blue-200 data-[owned]:left-full data-[owned]:-translate-x-full w-fit max-w-2/3"
 >
     @if(!$messageOwned)
         <h1 class="font-semibold">{{$message->author->name}}</h1>
     @endif
     <p>{{$message->content}}</p>
-    <h2 class="absolute bottom-0 opacity-80 translate-y-full text-end @if($messageOwned) right-0 @else left-0 @endif">
+    <h2 class="absolute bottom-0 block whitespace-nowrap opacity-80 translate-y-full text-end @if($messageOwned) right-0 @else left-0 @endif">
         {{$message->created_at->format('F j, Y g:i a')}}
     </h2>
 </div>
