6b40fc2ac90d9f54c09920d5a4c02b3d4dedcf92
Alex <alex@nebulasys.info>
2025-07-08 22:09:10 +0200

    Add HTMX CDN tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - Marktplaats</title>
+    <meta name="_token" content="{{ csrf_token() }}">
     <script src="https://unpkg.com/lucide@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js" integrity="sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css'])
 </head>
 
